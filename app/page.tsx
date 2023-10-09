fba6d08e88247cc274ab5f23a17b314b69e85610
steinhardt21 <alexandru.enache@protonmail.com>
2023-10-09 17:22:03 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,163 @@
+'use client'
 import Image from 'next/image'
+import axios from 'axios'
+import FormData from "form-data";
+import { useState } from 'react';
+
+const config = {
+  headers: {
+    "x-api-key": "sec_9FrP2zFJXxYWuXQX1tmY5sLv6OysV7Iy",
+    "Content-Type": "application/json",
+  },
+};
+
+const data = {
+  url: "https://www.glencore.com/.rest/api/v1/documents/static/a6349da6-3d11-4662-9107-28e19667d236/GLEN-2023-Half-Year-Report.pdf",
+};
+
 
 export default function Home() {
+  const [pdf, setPdf] = useState(null);
+  const [text, setText] = useState(""); // Added state for text input
+  const [output, setOutput] = useState("")
+
+  const handleClick = async () => {
+    const response = await axios.post(
+      "https://api.chatpdf.com/v1/sources/add-url",
+      data,
+      config
+    );
+    console.log(response);
+    const { data: { sourceId } } = response;
+    setPdf(sourceId);
+  };
+
+  const askPdf = async () => {
+    const data = {
+      sourceId: pdf,
+      messages: [
+        {
+          role: "user",
+          content: text
+        },
+      ],
+    };
+
+    console.log('text ', text)
+
+    const response = await axios.post(
+      "https://api.chatpdf.com/v1/chats/message",
+      data,
+      config
+    );
+
+
+    console.log("Result: ", response.data.content);
+    setOutput(response.data.content)
+  }
+
+
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (e: any) => {
+    setFile(e.target.files?.[0]);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!file) return
+
+    // uploadFile(file)
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const apiKey = "sec_9FrP2zFJXxYWuXQX1tmY5sLv6OysV7Iy";
+    const headers = {
+      "x-api-key": apiKey,
+    };
+
+    axios
+      .post("https://api.chatpdf.com/v1/sources/add-file", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "x-api-key": "sec_9FrP2zFJXxYWuXQX1tmY5sLv6OysV7Iy",
+        },
+      })
+      .then((response) => {
+        // handle the response
+        console.log(response);
+        setPdf(response.data.sourceId)
+      })
+      .catch((error) => {
+        // handle errors
+        console.log(error);
+      });
+  };
+
+
+  const uploadFile = async (file: any) => {
+    const chunkSize = 1024 * 1024; // 1MB chunks (adjust as needed)
+    const totalChunks = Math.ceil(file.size / chunkSize);
+    const apiKey = "sec_9FrP2zFJXxYWuXQX1tmY5sLv6OysV7Iy";
+
+    for (let chunkIndex = 0; chunkIndex < totalChunks; chunkIndex++) {
+      const start = chunkIndex * chunkSize;
+      const end = Math.min(start + chunkSize, file.size);
+      const chunk = file.slice(start, end);
+
+      const formData = new FormData();
+      formData.append("file", chunk);
+
+      try {
+        const response = await axios.post(
+          "https://api.chatpdf.com/v1/sources/add-file",
+          formData,
+          {
+            headers: {
+              "x-api-key": apiKey,
+              // Set the content type to multipart/form-data
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+
+        console.log(`Chunk ${chunkIndex + 1}/${totalChunks} uploaded.`);
+      } catch (error: any) {
+        console.error(`Error uploading chunk ${chunkIndex + 1}:`, error.message);
+        return;
+      }
+    }
+
+    console.log("File upload completed.");
+  };
+
+
+  const handleTextChange = (e: any) => {
+    setText(e.target.value); // Update the 'text' state when the input changes
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <input type="file" onChange={handleFileChange} />
+        <button type="submit">Upload</button>
+      </form>
+      <textarea
+        placeholder="Enter text here"
+        value={text}
+        onChange={handleTextChange}
+        style={{
+          color: 'black',
+          fontSize: '16px',
+          width: '100%',
+          minHeight: '100px', // Set the minimum height here
+          resize: 'vertical', // Allow vertical resizing
+        }}
+        rows={4} // Specify the number of visible rows
+      />
+      <button onClick={askPdf}>Ask</button>
+      <p style={{ color: 'white' }}>{output}</p>
     </main>
   )
 }
